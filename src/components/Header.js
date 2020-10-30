23dd2cc4c316ed64d40ff83dd098e3017922e392
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-10-30 11:12:16 +0000

    Adds new banner and alternative

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Image from './presentsplanner.png';
+import Image2 from './presentsplanner3.png';
+import Image6 from './presentsplanner6/png';
 
 const Header = () => {
 
     return (
         <div>
-            <img id="peonies" src={Image} />
+            <img id="peonies" src={Image6} />
         </div>
     )
 }
